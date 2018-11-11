422166fabccadfaa68b765716b089ea90dd253cc
Tiago Alves <tralves@gmail.com>
2018-11-11 20:47:05 +0000

    first commit

---
+++ README.md
@@ -0,0 +1,19 @@
+# android res image cover issue
+
+> App to demo a bug with images from res on Android.
+
+## Usage
+
+``` bash
+# Install dependencies
+npm install
+
+# Build for production
+tns build <platform> --bundle
+
+# Build, watch for changes and debug the application
+tns debug <platform> --bundle
+
+# Build, watch for changes and run the application
+tns run <platform> --bundle
+```
---
+++ app/components/App.vue
@@ -0,0 +1,31 @@
+<template>
+  <Page class="page" actionBarHidden="true" backgroundColor="#167dc3">
+    <StackLayout>
+      <Label width="200" height="200" text="huge" class="huge"/>
+      <Label width="200" height="200" text="medium" class="medium"/>
+      <Label width="100" height="100" text="small" class="small"/>
+    </StackLayout>
+  </Page>
+</template>
+
+<script>
+export default {}
+</script>
+
+<style lang="scss" scoped>
+label {
+  background-repeat: no-repeat;
+  background-size: cover;
+  background-position: center center;
+  margin: 10;
+}
+.huge {
+  background-image: url('res://chesshuge');
+}
+.medium {
+  background-image: url('res://chessmedium');
+}
+.small {
+  background-image: url('res://chesssmall');
+}
+</style>
